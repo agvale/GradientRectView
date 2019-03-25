e8a2cb12ac23f5e64cf81fbb32e6c93929a9bcec
wangzhiang <agvale@aliyun.com>
2019-03-25 14:15:25 +0800

    Initial Commit

---
+++ ZAGradientRectView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ZAGradientRectView
+//
+//  Created by zhiang on 2019/3/25.
+//  Copyright © 2019 fugui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
